6e914ee6a2dfd2ed89b3da188a83323a8c782caa
jiajunshen <shenjiajun90@gmail.com>
2015-02-22 10:01:55 -0600

    new extension pooling

--- mnist/trainExtensionPoolingModel.py
+++ mnist/trainExtensionPoolingModel.py
@@ -27,10 +27,13 @@ if pnet.parallel.main(__name__):
     parser.add_argument('poolingShape', metavar = '<pooling patch size>', type=int, help='size of pooling')
     parser.add_argument('data',metavar='<mnist data file>',type=argparse.FileType('rb'), help='Filename of data file')
     parser.add_argument('saveFile', metavar='<part Model file>', type=argparse.FileType('wb'),help='Filename of savable model file')
-    parser.add_argument('saveWeightsFile', metavar='<pooling weights file>', type=argparse.FileType('rb'), help='Filename of weights file')
+    parser.add_argument('saveWeightsFile', metavar='<pooling weights file>', type=argparse.FileType('wb'), help='Filename of weights file')
     parser.add_argument('poolingDistance',metavar='<distance of pooling>', type=int,help='Distance of Pooling')
     parser.add_argument('seed', metavar='<training seed>', type=int, help='training seed')
-
+    parser.add_argument('new_seed',metavar = '<random seed>', type=int, help = 'random seed')
+    parser.add_argument('epochs',metavar = '<number of epochs>', type=int, help = 'random seed')
+    parser.add_argument('n_hidden',metavar = '<number of hidden nodes>', type=int, help = 'random seed')
+    parser.add_argument('rate',metavar = '<rate>', type=int, help = 'random seed')
     args = parser.parse_args()
     model = args.model
     numParts = args.numParts
@@ -41,16 +44,20 @@ if pnet.parallel.main(__name__):
     extensionPatchSize = args.extensionPatchSize
     data = np.load(dataFileName)
     training_seed = args.seed
+    random_seed = args.new_seed
     net = pnet.PartsNet.load(args.model)
     pooling_distance = args.poolingDistance
     pooling_shape = args.poolingShape
+    epochs = args.epochs
+    n_hidden = args.n_hidden
+    rate = args.rate/100.0
 
     print(model, numParts, numExtensionParts, dataFileName, saveFile, weightsSaveFile, extensionPatchSize, training_seed, pooling_distance, pooling_shape)
     print("Inside")
     extensionlayers = [
         #pnet.ExtensionPartsLayer(num_parts = numParts, num_components = numExtensionParts, part_shape = (extensionPatchSize,extensionPatchSize), lowerLayerShape = (6,6))
         pnet.PoolingLayer(shape=(pooling_shape,pooling_shape),strides=(pooling_shape,pooling_shape)),
-        pnet.ExtensionPoolingLayer(n_parts = numParts * numExtensionParts, grouping_type='rbm', pooling_type='distance',pooling_distance=pooling_distance, save_weights_file = None, weights_file = weightsSaveFile, settings={})
+        pnet.ExtensionPoolingLayer(n_parts = numParts * numExtensionParts, grouping_type='rbm', pooling_type='distance',pooling_distance=pooling_distance, save_weights_file = weightsSaveFile, weights_file = None, settings=dict(n_hidden = n_hidden, epochs = epochs, rate = rate, rbm_seed = random_seed))
     ]
     clnet = pnet.PartsNet([net] + extensionlayers)
 
